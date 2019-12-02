7fc2db6056db18c6ebc6da1d1c4b3a46406bfc65
rajat0803 <rajat.batra@accoliteindia.com>
2019-12-02 12:39:33 +0530

    remove extra line

--- src/main/java/com/erstudio/ERStudioReleaseHelper.java
+++ src/main/java/com/erstudio/ERStudioReleaseHelper.java
@@ -13,10 +13,8 @@ public class ERStudioReleaseHelper {
      * Launches ERStudio Helper Application
      * @param args
      */
-
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         ERSStudioReleaseHelperUI ERStudioReleaseHelperUI = new ERSStudioReleaseHelperUI();
     }
-
 }
