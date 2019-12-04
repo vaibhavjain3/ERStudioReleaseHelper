d66933e150a1d3f7552fbc57beb1f48c1962f6ff
rajat0803 <rajat.batra@accoliteindia.com>
2019-12-04 14:05:49 +0530

    backupfiles logic

--- src/main/java/com/erstudio/tsversionchange/ChangeTSVersionNumber.java
+++ src/main/java/com/erstudio/tsversionchange/ChangeTSVersionNumber.java
@@ -1,13 +1,16 @@
 package main.java.com.erstudio.tsversionchange;
 
 import main.java.com.erstudio.constants.Constants;
+import main.java.com.erstudio.tsversionchange.Exception.VersionChangeException;
 import main.java.com.erstudio.tsversionchange.model.VersionInputModel;
 import main.java.com.erstudio.tsversionchange.model.VersionFormat;
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,7 +29,23 @@ public class ChangeTSVersionNumber {
 
     public List<String> changeVersion(VersionInputModel versionInputModel) {
         List<String> response = new ArrayList<>();
-        processExcel.getProcessedExcelList().stream().forEach(row ->
+
+        // back up all files
+        processExcel.getProcessedExcelList().forEach(row -> {
+            String directory = row.getFilepath().substring(0, row.getFilepath().lastIndexOf("\\"));
+            Path path = Paths.get(versionInputModel.getFilePath() + "\\backup\\" + directory);
+            try {
+                if (!Files.exists(path))
+                    Files.createDirectories(path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String source = versionInputModel.getFilePath() + row.getFilepath();
+            String target = versionInputModel.getFilePath() + "\\backup" + row.getFilepath();
+            backUpFiles(source, target);
+        });
+
+        processExcel.getProcessedExcelList().forEach(row ->
         {
             VersionFormat oldVersion = MapVersionFormat(versionInputModel.getOldVersion());
             VersionFormat newVersion = MapVersionFormat(versionInputModel.getNewVersion());
@@ -36,17 +55,25 @@ public class ChangeTSVersionNumber {
             String newVersionString = versionFormat.replace("Major", newVersion.getMajorVersion()).replace("Minor", newVersion.getMinorVersion())
                     .replace("Patch", newVersion.getRevisionNumber()).replace("BuildNumberUpdate", newVersion.getBuildNumber());
             String filepath = versionInputModel.getFilePath() + row.getFilepath();
-            replaceVersionInFile(filepath, oldVersionString, newVersionString, response);
+            try {
+                replaceVersionInFile(filepath, oldVersionString, newVersionString, response);
+            } catch (VersionChangeException e) {
+                e.printStackTrace();
+                response.add("restoring backup files...");
+                restoreBackUpFiles(versionInputModel);
+                response.add("all files are rolled back.");
+                return;
+            }
         });
         return response;
     }
 
-    public static VersionFormat MapVersionFormat(String version) {
+    private VersionFormat MapVersionFormat(String version) {
         String[] versionArr = version.split("\\.");
         return new VersionFormat(versionArr[0], versionArr[1], versionArr[2], versionArr[3]);
     }
 
-    public static List<String> replaceVersionInFile(String filepath, String search, String replace, List<String> response) {
+    private List<String> replaceVersionInFile(String filepath, String search, String replace, List<String> response) throws VersionChangeException {
         Path path = Paths.get(filepath);
         Charset charset = StandardCharsets.UTF_8;
 
@@ -58,7 +85,26 @@ public class ChangeTSVersionNumber {
             response.add("Successfully changed version in file " + filepath);
         } catch (Exception e) {
             response.add("Error in changing version in file " + filepath);
+            throw new VersionChangeException(e.getMessage());
         }
         return response;
+    }
+
+    private void backUpFiles(String sourcePath, String targetPath) {
+        Path source = Paths.get(sourcePath);
+        Path target = Paths.get(targetPath);
+        try {
+            Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    private void restoreBackUpFiles(VersionInputModel versionInputModel) {
+        this.processExcel.getProcessedExcelList().forEach(row -> {
+            String source = versionInputModel.getFilePath() + "\\backup" + row.getFilepath();
+            String target = versionInputModel.getFilePath() + row.getFilepath();
+            backUpFiles(source, target);
+        });
     }
 }
