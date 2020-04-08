f8e93d8132a9a80e7d30cdda1329c7e968a52102
shreypasari-accolite <shrey.pasari@accoliteindia.com>
2020-04-08 21:47:19 +0530

    Add new version in AbstractDatabaseCreator.java

--- src/main/java/com/erstudio/constants/Constants.java
+++ src/main/java/com/erstudio/constants/Constants.java
@@ -40,4 +40,5 @@ public class Constants {
     public static final String DA_FILE_ERDTREPO = "ERDTRepo.sln";
     public static final String OPERATION_TYPE_VERSION_CHANGE = "Version Change";
     public static final String OPERATION_TYPE_GUID_UPDATE = "GUID Update";
+    public static final String OPERATION_TYPE_ADD_VERSION = "Add Version";
 }
